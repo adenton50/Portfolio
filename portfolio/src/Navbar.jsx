ad01a1879ee149324ace99a310d9d0192abc0950
adam denton <adamdenton50@gmail.com>
2024-08-02 00:17:41 -0500

    working on about component

---
+++ portfolio/src/Navbar.jsx
@@ -0,0 +1,22 @@
+export default function Navbar() {
+  return (
+    <div>
+      <nav className="flex items-center justify-center bg-gray-700 h-16">
+        <ul className="flex flex-row justify-evenly w-1/2 font-medium text-lg text-white ">
+          <a href="#">
+            <li className="hover:text-red-500">Home</li>
+          </a>
+          <a href="#">
+            <li className="hover:text-red-500">About</li>
+          </a>
+          <a href="#">
+            <li className="hover:text-red-500">Projects</li>
+          </a>
+          <a href="#">
+            <li className="hover:text-red-500">Contact</li>
+          </a>
+        </ul>
+      </nav>
+    </div>
+  );
+}
--- portfolio/src/Welcome.jsx
+++ portfolio/src/Welcome.jsx
@@ -6,12 +6,26 @@ export default function Welcome({ setCurrentSection }) {
       <Title />
       <button
         type="button"
-        className="border-4 border-red-600 rounded-md px-8 py-4 text-red-600 text-xl hover:shadow-red-2xl transition-shadow duration-300"
+        className="flex flex-row items-center gap-2 border-4 border-red-600 rounded-md px-8 py-4 text-red-500 text-xl hover:shadow-red-2xl transition-shadow duration-300"
         onClick={() => {
           setCurrentSection("about");
         }}
       >
         View my work
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3"
+          />
+        </svg>
       </button>
     </div>
   );
