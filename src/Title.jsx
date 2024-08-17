788e78e549410724441d9fc4bf686ba4a04ad417
adam denton <adamdenton50@gmail.com>
2024-08-16 21:43:00 -0500

    edit font size and px

--- src/Title.jsx
+++ src/Title.jsx
@@ -3,7 +3,7 @@ import { ReactTyped } from "react-typed";
 export default function Title() {
   return (
     <div>
-      <h1 className="text-white text-3xl md:text-5xl mb-16 text-center font-typewriter">
+      <h1 className="text-white text-2xl md:text-5xl mb-16 text-center font-typewriter">
         <ReactTyped
           strings={[
             "Hello, I'm <span>Adam</span>. I'm an aspiring front end developer.",
--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -3,11 +3,11 @@ import Title from "./Title";
 
 export default function Welcome({ setCurrentSection }) {
   return (
-    <div className="flex flex-col justify-center items-center mx-auto mt-56 md:mt-96">
+    <div className="flex flex-col justify-center items-center mx-auto mt-64 md:mt-96 px-8">
       <Title />
       <button
         type="button"
-        className="flex flex-row items-center gap-2 border-4 border-orange-600 rounded-md px-8 py-4 text-orange-500 text-xl hover:shadow-red-2xl transition-shadow duration-300"
+        className="flex flex-row items-center gap-2 border-4 border-orange-600 rounded-md px-4 py-2 md:px-8 md:py-4 text-orange-500 text-xl hover:shadow-red-2xl transition-shadow duration-300"
         onClick={() => {
           setCurrentSection("about");
         }}
